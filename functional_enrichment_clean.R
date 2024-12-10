9a50e7cdf6f89ab6465c1edbb2bd5f88d7b4fe38
MangezDesPomm3s <esteban.coupriediaz@yahoo.com>
2024-12-10 22:05:35 +0100

    Reworked functional_enrichment_clean.R
    -I've completly reworked the DESeq call and made it clearer to understand for others. In the previous version it was impossible to adress individual timepoints with the way the metadata was structured
    -Simplified bar plot function and used gene_ratio and not gene count
    -Added Bubble Plot function and corresponding loop

--- functional_enrichment_clean.R
+++ functional_enrichment_clean.R
@@ -1,175 +1,103 @@
-# Install necessary packages if you haven't already
-install.packages("cowplot")
-install.packages("gplots")
-devtools::install_github("nicolash2/ggvenn")
-BiocManager::install("DESeq2")  # if DESeq2 is not already installed
-install.packages("dplyr")
-install.packages("ggplot2")
-install.packages("stringr")
-
 # Load libraries
-library("matrixTests")
 library("ggrepel")
 library("enrichR")
-library("cowplot")
 library("ggplot2")
-library("MASS")
-library("ggvenn")
 library("gplots")
 library("DESeq2")
 library("stringr")
+library("dplyr")
+library('cowplot')
+library("reshape2")
 
-# Set working direction
-setwd("D:/Documents/Rstudio/Project-SARSHuman/")
+# Set working directory
+setwd("F:/Documents/Rstudio/Project-SARSHuman/")
 
 #Creation Dataframe
 data = read.delim("./01_data/project-SARSHuman.txt" ,sep = "\t")
 
+###############################
+###DESeq Enrichment analysis###
+###############################
+
 # Define the timepoints for each condition
-sars_timepoints <- c(0, 12, 24, 36, 48, 60, 72, 84, 94) 
+sars_timepoints <- c(0, 12, 24, 36, 48, 60, 72, 84, 96) 
 h1n1_timepoints <- c(0, 6, 12, 18, 24, 36, 48)           
 
-# List of all the conditions you have
+# List of all the conditions you have and a vector without control
 conditions <- c("SARS.Cov", "SARS.dORF6", "SARS.BatSRBD", "H1N1", "mock")
+conditions_to_compare <- c("SARS.Cov", "SARS.dORF6", "SARS.BatSRBD", "H1N1")
 
 # Sample names
 sample_names <- colnames(data)  # This should be your sample names like "H1N1_00h_1"
 
-# Extract condition and timepoint information from sample names
-# Create a vector to store the conditions
+# Create a vector to store for every conditions as name, the virus tested
 condition <- sapply(sample_names, function(x) strsplit(x, "_")[[1]][1])  # First part is condition (e.g., H1N1)
-# Create a vector to store the timepoints
+# Create a vector to store fore every condition as name, the timepoints
 timepoint <- sapply(sample_names, function(x) strsplit(x, "_")[[1]][2])  # Second part is timepoint (e.g., 00h)
-
-# Remove the 'h' from timepoints and convert to numeric values
-timepoint_numeric <- as.numeric(gsub("h", "", timepoint))
+timepoint_numeric <- as.numeric(gsub("h", "", timepoint)) # Remove the 'h' from timepoints and convert to numeric values
 
 # Create the metadata dataframe with replicates grouped by condition and timepoint
 metadata <- data.frame(
-  condition = factor(condition, levels = conditions),  # Condition as a factor
-  timepoint = timepoint_numeric  # Timepoint as numeric
+  condition = factor(condition, levels = conditions),  #The vector Conditions is used as a factor
+  timepoint = timepoint_numeric # Timepoint as numeric
 )
 
-# Ensure that metadata has the same number of rows as the columns in the data
-rownames(metadata) <- sample_names
-
-# Liste des conditions et points de temps disponibles pour chaque condition
-conditions_to_compare <- c("SARS.Cov", "SARS.dORF6", "SARS.BatSRBD", "H1N1")
-mock_timepoints <- c(0, 12, 24, 36, 48, 60, 72, 84, 94)  # Points de temps pour le groupe contrôle (mock)
-
-# Créer une liste vide pour stocker les résultats
-results_list <- list()
+# Column condition.timepoint allow to factor the 3 replicats of a condition for each timepoint
+metadata$condition_timepoint = interaction(metadata$condition, metadata$timepoint, sep = ".")
 
 # Creating DESeq2 dataset from count data and metadata
-dds <- DESeqDataSetFromMatrix(countData = round(data), colData = metadata, design = ~ condition + timepoint)
-
-# Check the DESeq2 object
-dds
+dds <- DESeqDataSetFromMatrix(countData = round(data), colData = metadata, design = ~ condition_timepoint) #Allows to compare between different condition and adress each timepoint
 
 # Run the DESeq analysis
 dds <- DESeq(dds)
 
-#Comparing the data to control####
-contrast = c("variable", "level1", "level2")
+######################################
+###Creation of enrichment Dataframe###
+######################################
 
-#Create a vector of conditions to compare with Control
-conditions_to_compare <- c("SARS.Cov", "SARS.dORF6", "SARS.BatSRBD", "H1N1")
-
-#Functional_enr database
+# We create empty dataframe with genes as row names
 func_enr_data <- data.frame(row.names = rownames(dds))
 
-#We create a dataframe containning every enriched genes as 1 and others as 0 in every condition for the enrichedR package
-
-for (condition in conditions_to_compare) {
-  for (time in unique(metadata$timepoint)) {
-    condition_samples <- grep(paste0(condition, "_", time, "h"), sample_names)
-    mock_samples <- grep(paste0("mock_", time, "h"), sample_names)
-    if (length(condition_samples) > 0 && length(mock_samples) > 0) {
-      res <- results(dds, contrast = c("condition", condition, "mock"))
-      if (!is.null(res)) {
-        res_timepoint <- res[metadata$timepoint == time, ]
-        if (!is.null(res_timepoint)) {
-          temp_data <- as.data.frame(res_timepoint)
-          print(paste(condition,time))
-          temp_data$enriched = 0
-          temp_data$enriched[temp_data$pvalue<0.05] = 1
-          
-          df = temp_data['enriched']
-          names(df)[names(df) == 'enriched'] <- paste(condition,'_',time,'h',sep='')
-          
-          func_enr_data <- merge(func_enr_data, df, by = "row.names", all = TRUE)
-          
-          rownames(func_enr_data) <- func_enr_data$Row.names #Magic trick
-          func_enr_data$Row.names <- NULL
-        }
-      }
-    }
+# we loop so that every enriched genes appears as 1 or -1 and others as 0 in every condition
+for(conds in conditions_to_compare) {
+  time_vect <- c() # We ensure time points exists for each condition with following if()
+  
+  if(conds %in% c("SARS.Cov", "SARS.dORF6", "SARS.BatSRBD")) {
+    time_vect = sars_timepoints
+  }else{
+    time_vect = h1n1_timepoints
+  }
+  
+  for(time in time_vect) { 
+    temp_data <- data.frame()
+    
+    #The comparaison is made bewteen a condition i.e SARS.Cov_24h and Mock_24h for example###
+    comp <- c("condition_timepoint", paste0(conds, ".", time), paste0("mock.", time)) #Creates the comparison vector for the given condition and time point to give to DESeq2 function
+    
+    res <- results(dds, contrast = comp) #Extract DESeq results from dds for this comparison
+    temp_data <- as.data.frame(res) # Convert DESeq result to dataframe to extract the relevant info
+    
+    print(paste0(conds,'_',time,'h')) # Keeps track of function
+    
+    ###GATING PARAMETERS###
+    temp_data$enriched = 0
+    temp_data$enriched[(temp_data$padj<0.05) & (temp_data$log2FoldChange > 0)] = 1 # Append column in res dataframe based on p. value adjusted
+    temp_data$enriched[(temp_data$padj<0.05) & (temp_data$log2FoldChange < 0)] = -1
+    
+    func_enr_data[paste0(conds,'_',time,'h')] = temp_data$enriched # Append results to the global func_enr_data dataframe
   }
 }
 
+#Saving results
+write.table(func_enr_data,file = "./01_data/func_enr_data_test.txt",sep = "\t",quote = FALSE)
 
 
-#Gene count ggplot TEST###
-fe_plot = function(genenames,subtitle="",database_slc=''){
-  
-  #Vector of selectable databases
-  dbs <- c("GO_Molecular_Function_2023",
-           "GO_Cellular_Component_2023",
-           "GO_Biological_Process_2023",
-           "Human_Gene_Atlas",
-           "BioPlanet_2019")
-  
-  enriched <- enrichr(genenames, dbs)
-  
-  fe_df = enriched[[database_slc]]
-  fe_df = fe_df[order(fe_df$Adjusted.P.value,decreasing = FALSE),]
-  fe_df = fe_df[1:15,] #select 15 most significant pathway enriched
-  
-  fe_df$Term = factor(fe_df$Term,levels = rev(fe_df$Term))
-  fe_df$label <- str_wrap(fe_df$Term, width = 40, exdent = 5,whitespace_only=FALSE) #str_trunc(fe_df$Term, width = 40)  #
-  
-  
-  fe_df$gene_count = ifelse(fe_df$P.value < 0.05, str_count(fe_df$Genes, ";"), 0)
-  fe_df$Adjusted.P.value = fe_df$Adjusted.P.value * 0.01
-  
-  plot = ggplot() +
-    labs(title="",
-         subtitle = paste(subtitle,'enrichment vs mock'), #Change paste statement for regina's data
-         tag="") +
-    geom_col(data=fe_df,aes(y=Term,
-                            x= pmin(pmax(gene_count, 0), 30), #Squishes out of bounds Values
-                            fill=Adjusted.P.value)) +
-    geom_point(
-      data = fe_df %>% filter(gene_count < 0 | gene_count > 30),  # Filter out-of-bound values
-      aes(x = pmin(pmax(gene_count, 0), 30), y = Term),  # Plot at squished positions
-      shape = 4,  # Cross marker for special ticks
-      size = 3,
-      color = "gray40"
-    )+
-    scale_fill_gradient(low = "red", high="blue",
-                        oob = scales::squish,  # Include out-of-bounds values
-                        limits = c(0.001, 0.01),
-                        name = "Adj. P-value\n") +
-    scale_y_discrete(labels = fe_df$label,
-                     expand = expansion(mult = c(0.05, 0.05))) +
-    scale_x_continuous(limits = c(0,30)) +
-    xlab("Gene Count")+
-    ylab("") +
-    theme_bw() +
-    theme(plot.subtitle = element_text(face = "italic"),
-          legend.position = "right",
-          legend.title = element_text(size = 10),
-          legend.text = element_text(size = 8),
-          axis.text.y = element_text(size = 6,
-                                     lineheight = 0.8),
-          plot.margin = margin(t = 5,
-                               r = 30,
-                               b = 10,
-                               l = 10),
-          axis.ticks.length = unit(0.2, "cm"))
-}
+##########################
+###Function DEF section###
+##########################
 
-fe_plot_countsorted = function(genenames,subtitle="",database_slc=''){
+#Function creating a graph for each enriched gene name list for a given condition in an enrichR database
+fe_plot_bar_ratiosorted = function(genenames,subtitle="",database_slc=''){
   
   #Vector of selectable databases
   dbs <- c("GO_Molecular_Function_2023",
@@ -182,61 +110,198 @@ fe_plot_countsorted = function(genenames,subtitle="",database_slc=''){
   
   fe_df = enriched[[database_slc]]
   
-  fe_df$gene_count = ifelse(fe_df$P.value < 0.05, str_count(fe_df$Genes, ";"), 0)
-  fe_df$Adjusted.P.value = fe_df$Adjusted.P.value * 0.01
+  fe_df = fe_df[fe_df$Adjusted.P.value < 0.05,] # Subset the vector for only the significant patwhays
+  fe_df = fe_df[order(fe_df$Adjusted.P.value,decreasing = FALSE),] #Sort by Adjusted Pval
+ 
+  fe_df = fe_df[1:15,] #select 15 most significant pathway enriched -> Chose number of pathways to compare
   
-  fe_df = fe_df[order(fe_df$gene_count,decreasing = TRUE),]
-  fe_df = fe_df[1:15,] #select 15 most significant pathway enriched
+  fe_df$overlap_int = as.numeric(sapply(strsplit(fe_df$Overlap,split='/'), `[`, 1))
+  fe_df$totalpath_int = as.numeric(sapply(strsplit(fe_df$Overlap,split='/'), `[`, 2))
+  fe_df$gene_ratio = fe_df$overlap_int / fe_df$totalpath_int #Gene ratio is calculated by number of overlapping gene in a pathway divided by number in pathway
+  
   
   fe_df$Term = factor(fe_df$Term,levels = rev(fe_df$Term))
-  fe_df$label <- str_wrap(fe_df$Term, width = 40, exdent = 5,whitespace_only=FALSE) 
+  fe_df$label <- str_wrap(fe_df$Term, width = 40, exdent = 5,whitespace_only=FALSE) #Wraps y labels correctly
   
-  plot = ggplot() +
+  #Create Plot object for the function call
+  plot = ggplot() +     
     labs(title="",
          subtitle = paste(subtitle,'enrichment vs mock'), #Change paste statement for regina's data
          tag="") +
-    geom_col(data=fe_df,aes(y=Term,
-                            x= pmin(pmax(gene_count, 0), 30), #Squishes out of bounds Values
-                            fill=Adjusted.P.value)) +
-    geom_point(
-      data = fe_df %>% filter(gene_count < 0 | gene_count > 30),  # Filter out-of-bound values
-      aes(x = pmin(pmax(gene_count, 0), 30), y = Term),  # Plot at squished positions
-      shape = 4,  # Cross marker for special ticks
-      size = 3,
-      color = "gray40"
-    )+
+    geom_col(data=fe_df,aes(y=Term,x=gene_ratio,fill=Adjusted.P.value)) +
     scale_fill_gradient(low = "red", high="blue",
-                        oob = scales::squish,  # Include out-of-bounds values
-                        limits = c(0.001, 0.01),
+                        oob = scales::squish,  #Include out-of-bounds values
+                        limits = c(0.0001, 0.01),
                         name = "Adj. P-value\n") +
     scale_y_discrete(labels = fe_df$label,
                      expand = expansion(mult = c(0.05, 0.05))) +
-    scale_x_continuous(limits = c(0,30)) +
-    xlab("Gene Count")+
+    xlab("Gene Ratio")+
     ylab("") +
     theme_bw() +
     theme(plot.subtitle = element_text(face = "italic"),
           legend.position = "right",
           legend.title = element_text(size = 10),
           legend.text = element_text(size = 8),
-          axis.text.y = element_text(size = 6,
+          axis.text.y = element_text(size = 6, #Change this value to 8 for single and 6 for grid
                                      lineheight = 0.8),
           plot.margin = margin(t = 5,
                                r = 30,
                                b = 10,
                                l = 10),
           axis.ticks.length = unit(0.2, "cm"))
+    return(plot)
 }
+
+buble_plot = function(data_enriched,subtitle="",database_slc='') {
+  """Needs to be fed func_enr_data object in data_enr argument"""
+  
+  #Vector of selectable databases
+  dbs <- c("GO_Molecular_Function_2023",
+           "GO_Cellular_Component_2023",
+           "GO_Biological_Process_2023",
+           "Human_Gene_Atlas",
+           "BioPlanet_2019")
+  
+  enr_data = data_enriched[,subtitle]
+  idx = enr_data==1
+  genenames_up = rownames(data_enriched)[idx]
+  
+  idx = enr_data==-1 
+  genenames_down = rownames(data_enriched)[idx]
+  
+  enriched_up <- enrichr(genenames_up, dbs)
+  enriched_down <- enrichr(genenames_down, dbs)
+  
+  up = enriched_up[[database_slc]]
+  up = up[up$Adjusted.P.value < 0.05,] # Subset the vector for only the significant patwhays
+  
+  down = enriched_down[[database_slc]]
+  down = down[down$Adjusted.P.value < 0.05,] # Subset the vector for only the significant patwhays
+  
+  #Checks if the subset exist
+  if(nrow(up) > 0) {
+    up$regulation = 'Up-Regulated'
+    up$gene_count = str_count(up$Genes, ";") + 1
+    up$overlap_int = as.numeric(sapply(strsplit(up$Overlap,split='/'), `[`, 1))
+    up$totalpath_int = as.numeric(sapply(strsplit(up$Overlap,split='/'), `[`, 2))
+    up$gene_ratio = up$overlap_int / up$totalpath_int #Gene ratio is calculated by number of overlapping gene in a pathway divided by overall number in a given path
+    up = up[order(up$Adjusted.P.value,decreasing = FALSE),] #Sort By Combined.Score
+    ifelse(nrow(down)>0, up <- up[1:10,], up <- up[1:20,]) #select 15 most significant pathway enriched -> Chose number of pathways to compare
+  } else { 
+    #Placeholder Data
+    up <- data.frame(
+      Term = character(0),
+      regulation = character(0),
+      gene_count = numeric(0),
+      gene_ratio = numeric(0),
+      Adjusted.P.value = numeric(0))
+  }
+  
+  if(nrow(down) > 0) {
+    down$regulation = 'Down-Regulated'
+    down$gene_count = str_count(down$Genes, ";") + 1
+    down$overlap_int = as.numeric(sapply(strsplit(down$Overlap,split='/'), `[`, 1))
+    down$totalpath_int = as.numeric(sapply(strsplit(down$Overlap,split='/'), `[`, 2))
+    down$gene_ratio = down$overlap_int / down$totalpath_int #Gene ratio is calculated by number of overlapping gene in a pathway divided by overall number in a given path
+    down = down[order(down$Adjusted.P.value,decreasing = FALSE),] #Sort By Combined.Score
+    ifelse(nrow(up)>0, down <- down[1:10,], down <- down[1:20,]) #select 15 most significant pathway enriched -> Chose number of pathways to compare
+  } else {
+    #Placeholder Data
+    down <- data.frame(
+      Term = character(0),
+      regulation = character(0),
+      gene_count = numeric(0),
+      gene_ratio = numeric(0),
+      Adjusted.P.value = numeric(0))
+  }
+
+  #Creates buble data frame with both up and down regulated
+  buble <- rbind(up,down)
+  
+  #Omit NA rows if less than 10 pathways enriched for each
+  buble <- na.omit(buble)
+  
+  # In case Both Up and Down are empty and not significant
+  if (nrow(buble) == 0) {
+    message("No significant pathways found for either up- or down-regulated genes.")
+    return(ggplot() + ggtitle("No significant pathways to display"))
+  }
+  
+  #We Ensures Buble is also sorted by gene_ratio
+  buble <- buble[order(buble$gene_ratio,decreasing = FALSE),]
+  
+  # Order by gene_ratio
+  buble <- buble %>% mutate(Term_wrapped = str_wrap(Term, width = 40),
+                            Term_wrapped = factor(Term_wrapped, levels = unique(Term_wrapped))
+                            )
+
+  #We create a plot object for the Bubble plot
+  plot = ggplot(buble, aes(x = gene_ratio, y = Term_wrapped, size = gene_count, color = Adjusted.P.value)) +
+    geom_point() +
+    scale_color_gradient(low = "red", high = "blue") +
+    facet_wrap(~regulation) +
+    theme_bw() +
+    labs(
+      x = "Gene Ratio",
+      y = "",
+      size = "Gene Count",
+      color = "Adjusted P-value",
+      title = paste(subtitle,'enrichment vs mock')
+    ) + 
+    theme(
+      legend.position = "right",  # Place legends to the right
+      legend.title = element_text(size = 10),
+      legend.text = element_text(size = 8),
+      axis.text.y = element_text(size = 8, lineheight = 0.8),  # Adjust y-axis text
+      plot.subtitle = element_text(face = "italic"),
+      plot.margin = margin(t = 10,
+                           r = 10,
+                           b = 10,
+                           l = 10))
+  return(plot)
+}
+
+
+                    
+##########################
+###BUBLE PLOT CALL LOOP###
+##########################
+                 
+plt_buble_list = list() #List of buble plots
+
+#Open pdf file to write to
+pdf(file = "./03_plots/fe_BioPlanet_2019_VSmock_bubblesPlot.pdf", #Change Filename based on function used
+    width = 12,
+    height = 7)
+
+#Looping
+for(condition in colnames(func_enr_data)) {
+  print(condition)
+  
+  plot = buble_plot(func_enr_data,condition,'BioPlanet_2019')
+  plot(plot)
+  
+  plt_buble_list[[condition]] = plot #Saves plot to the environement
+  
+}
+dev.off()
+
+buble_plot(func_enr_data,"SARS.Cov_24h",'BioPlanet_2019')
+
+
+########################
+###BAR PLOT CALL LOOP###
+########################
 
 #list which containns every plot generated
 plt_list = list()
 
 #Open pdf file to write to
-pdf(file = "./03_plots/fe_BioPlanet_2019_VSmock_countsorted.pdf", #Change Filename based on function used
+pdf(file = "./03_plots/fe_BioPlanet_2019_VSmock_combsorted_test.pdf", #Change Filename based on function used
     width = 12,
     height = 7)
 
-#Execution plot loop###
+#Execution plot loop for barpplot###
 for(condition in colnames(func_enr_data)){
   print(condition)
   
@@ -244,19 +309,17 @@ for(condition in colnames(func_enr_data)){
   idx = values==1
   genenames = rownames(func_enr_data)[idx]
   
-  plot = fe_plot_countsorted(genenames,condition,'BioPlanet_2019')
-  plot(plot)
+  plot = fe_plot_bar_ratiosorted(genenames,condition,'BioPlanet_2019')
+  plot(plot) 
   
-  plt_list[[condition]] = plot
+  plt_list[[condition]] = plot #Saves plot to the environement
 }
 dev.off()
 
-
-#Creating grid plots###
-
+#Creating grid plots for bars from plt_list object###
 comparaisons <- c(12, 24, 36, 48)
 
-pdf(file = "./03_plots/grid_fe_BioPlanet_2019_VSmock_countsorted.pdf", #Change Filename based on function used
+pdf(file = "./03_plots/grid_fe_BioPlanet_2019_VSmock_combsorted_test.pdf", #Change Filename based on function used
     width = 12,
     height = 7)
 
